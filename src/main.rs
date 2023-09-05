780b4854778c2f82e22b026453b2193a80ef073d
girishnarayan <girishnarayan@outlook.it>
2023-09-05 15:45:10 +0200

    rust new

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+
+
+use core::panic::PanicInfo;
+use core::arch::asm;
+
+mod boot{
+    use core::arch::global_asm;
+    
+    global_asm!(
+        ".section .text._start"
+    );
+}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+
+unsafe{
+     core::ptr::write_volatile(0x3F20_0008 as *mut u32, 1<<3);
+     loop{
+
+        //turn pin on 
+        core::ptr::write_volatile(0x3F20_001C as *mut u32, 1<<21);
+        
+        for _ in 1..50000{
+            asm!("nop");
+        }
+
+        //turn pin off
+        core::ptr::write_volatile(0x3F20_0028 as *mut u32, 1<<21);
+        for _ in 1..50000{
+            asm!("nop");
+        }
+    }
+  }
+}
+
+#[panic_handler]
+fn panic (_info: &PanicInfo) -> ! {
+    loop{}
+}
